ab75b63d8b5cf8e07df11a940d49a7697b85eb94
M Saim Hashmii <saimhashmii433@gmail.com>
2021-10-04 23:17:47 +0500

    Simple Email Notification 1

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\User;
+use App\Notifications\TaskCompleted;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+
+   	// User::find(1)->notify(new TaskCompleted);
+
+   	// SEND NOTIFICATION VIA FACADE
+   	$users = User::find(1);
+   	Notification::send($users, new TaskCompleted());
+
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+
+require __DIR__.'/auth.php';
